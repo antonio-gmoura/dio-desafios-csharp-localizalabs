d9295699f7ae5b5ea5c48b59400fb1cac1c94b99
Antonio Moura <antonio.gmoura@gmail.com>
2022-02-12 23:36:05 -0400

    Desafio 1

---
+++ Desafio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                if (i < N) {
+                    Console.Write("Ho ");
+                }
+                else{
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
