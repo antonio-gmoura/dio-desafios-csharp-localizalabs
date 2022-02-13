c66a00d3da385a165cbc10fb35d08d9ac2b01e29
Antonio Moura <antonio.gmoura@gmail.com>
2022-02-13 00:23:10 -0400

    Desafio 2

---
+++ Desafio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++) 
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0) {
+                Console.WriteLine("divisao impossivel");
+                } else {
+                Console.WriteLine("{0:N1}", (X/Y));
+                }
+            }
+        }
+    }
+}
